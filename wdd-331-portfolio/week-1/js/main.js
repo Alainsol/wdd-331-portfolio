62257241bddee161447c48bbb05a574130335360
sol21002@byui.edu <85121697+Alainsol@users.noreply.github.com>
2022-05-05 01:52:02 +0700

    reinstall page

---
+++ wdd-331-portfolio/week-1/js/main.js
@@ -0,0 +1,29 @@
+const links = [
+    {
+      label: "Week-1",
+      url: "week-1/index.html"
+    },
+    {label: "Week-2",
+      url: "week-2/index.html"
+    },
+    {label: "Week-3",
+      url: "week-3/index.html"
+    }
+
+  ];
+
+
+
+
+
+  
+const worklist = document.getElementById('listwork');
+
+for(let i = 0; i < links.length; i++) {
+    const menu = document.createElement('li');
+    const link = document.createElement('a');
+    link.href = links[i].url;
+    link.innerHTML = links[i].label;
+    menu.appendChild(link);
+    worklist.appendChild(menu);
+}
---
+++ week-2/js/main.js
@@ -0,0 +1,22 @@
+const links = [
+    {
+      label: "Week-1",
+      url: "week-1/index.html"
+    }
+  ];
+
+
+
+
+
+  
+const worklist = document.getElementById('listwork');
+
+for(let i = 0; i < links.length; i++) {
+    const menu = document.createElement('li');
+    const link = document.createElement('a');
+    link.href = links[i].url;
+    link.innerHTML = links[i].label;
+    menu.appendChild(link);
+    worklist.appendChild(menu);
+}
